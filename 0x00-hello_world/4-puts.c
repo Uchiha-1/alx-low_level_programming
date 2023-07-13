e321495f9752d31def2e669cf7556bf5c0eefad6
uchiha <210503012@live.unilag.edu.ng>
2023-07-13 16:17:28 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+/*puts inffo
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
-int main (void)
+/* using printf to print
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return	(0);
