9930fdd25d182f1a0f2b4efdff59a6b84fcb6a1c
uchiha <210503012@live.unilag.edu.ng>
2023-07-13 17:04:13 -0700

    alx

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>/*begining*/
-/*main*/
-int main(void)/*main love*/
+
+/*main*/int main(void)/*main love*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return	(0);
